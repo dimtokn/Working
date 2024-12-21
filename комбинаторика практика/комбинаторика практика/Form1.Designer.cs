b14d7a7ff9c28413f63154cdf12e758ad16956ec
dimtokn <tokarev.dima777@gmail.com>
2024-12-21 11:02:13 +0500

    Практика 1
    Комбинаторика

---
+++ комбинаторика практика/комбинаторика практика/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace CombinatoricsCalculator
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private TextBox txtN;
+        private TextBox txtK;
+        private ComboBox cmbOperation;
+        private Button btnCalculate;
+        private Label lblResult;
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.txtK = new System.Windows.Forms.TextBox();
+            this.cmbOperation = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(30, 31);
+            this.txtN.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(148, 26);
+            this.txtN.TabIndex = 0;
+            // 
+            // txtK
+            // 
+            this.txtK.Location = new System.Drawing.Point(30, 92);
+            this.txtK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtK.Name = "txtK";
+            this.txtK.Size = new System.Drawing.Size(148, 26);
+            this.txtK.TabIndex = 1;
+            // 
+            // cmbOperation
+            // 
+            this.cmbOperation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperation.Location = new System.Drawing.Point(30, 154);
+            this.cmbOperation.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbOperation.Name = "cmbOperation";
+            this.cmbOperation.Size = new System.Drawing.Size(298, 28);
+            this.cmbOperation.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(28, 216);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(150, 46);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Рассчитать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Location = new System.Drawing.Point(30, 277);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(450, 46);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Результат: ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 385);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.txtK);
+            this.Controls.Add(this.cmbOperation);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblResult);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Комбинаторика";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ комбинаторика практика/комбинаторика практика/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CombinatoricsCalculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(txtN.Text);
+                int k = int.Parse(txtK.Text);
+                string operation = cmbOperation.SelectedItem.ToString();//выбор операции
+                double result = 0;
+
+                switch (operation)
+                {
+                    case "Перестановка без повторения":
+                        result = Factorial(n);
+                        // Перестановка без повторения: P(n) = n!
+                        break;
+
+                    case "Перестановка с повторением":
+                        result = Math.Pow(n, n);
+                        // Перестановка с повторением: P= n^n
+                        break;
+
+                    case "Сочетание без повторения":
+                        result = Factorial(n) / (Factorial(k) * Factorial(n - k));
+                        // Сочетание без повторения: C= n! / (k! * (n-k)!)
+                        break;
+
+                    case "Сочетание с повторением":
+                        result = Factorial(n + k - 1) / (Factorial(k) * Factorial(n - 1));
+                        // Сочетание с повторением: C = (n+k-1)! / (k! * (n-1)!)
+                        break;
+
+                    case "Размещение без повторения":
+                        result = Factorial(n) / Factorial(n - k);
+                        // Размещение без повторения: A = n! / (n-k)!
+                        break;
+
+                    case "Размещение с повторением":
+                        result = Math.Pow(n, k);
+                        // Размещение с повторением: A = n^k
+                        break;
+
+                    default:
+                        MessageBox.Show("Выберите операцию.");//если операция не выбрана
+                        return;
+                }
+
+                lblResult.Text = $"Результат: {result}";//вывод результата
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");//если не ввести значение and k
+            }
+        }
+
+        private double Factorial(int x)
+        {
+            // Факториал числа: n! = 1 * 2 * ... * n
+            if (x <= 1) return 1;
+            return x * Factorial(x - 1);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Добавление операций в выпадающий список при загрузке формы
+            cmbOperation.Items.AddRange(new string[]
+            {
+                "Перестановка без повторения",
+                "Перестановка с повторением",
+                "Сочетание без повторения",
+                "Сочетание с повторением",
+                "Размещение без повторения",
+                "Размещение с повторением"
+            });
+
+            cmbOperation.SelectedIndex = 0; // Выбирается первый элемент по умолчанию
+        }
+    }
+}

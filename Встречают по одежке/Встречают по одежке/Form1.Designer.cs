04a28ce0ce88113376bfaea97d1b88863506c4c1
dimtokn <tokarev.dima777@gmail.com>
2024-12-21 11:20:52 +0500

    Практика 5
    ООО "Встречают по одежке"

---
+++ Встречают по одежке/Встречают по одежке/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Встречают_по_одежке
+{
+    public partial class Form1 : Form
+    {
+        private Label labelFlight;
+        private ComboBox comboBoxFlights;
+        private Button buttonCalculate;
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelFlight = new System.Windows.Forms.Label();
+            this.comboBoxFlights = new System.Windows.Forms.ComboBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.listBoxTickets = new System.Windows.Forms.ListBox();
+            this.labelClass = new System.Windows.Forms.Label();
+            this.comboBoxClass = new System.Windows.Forms.ComboBox();
+            this.pictureBoxProduct = new System.Windows.Forms.PictureBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelFlight
+            // 
+            this.labelFlight.AutoSize = true;
+            this.labelFlight.Location = new System.Drawing.Point(12, 15);
+            this.labelFlight.Name = "labelFlight";
+            this.labelFlight.Size = new System.Drawing.Size(128, 20);
+            this.labelFlight.TabIndex = 0;
+            this.labelFlight.Text = "Выбор одежды:";
+            // 
+            // comboBoxFlights
+            // 
+            this.comboBoxFlights.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFlights.Location = new System.Drawing.Point(457, 12);
+            this.comboBoxFlights.Name = "comboBoxFlights";
+            this.comboBoxFlights.Size = new System.Drawing.Size(200, 28);
+            this.comboBoxFlights.TabIndex = 1;
+            this.comboBoxFlights.SelectedIndexChanged += new System.EventHandler(this.comboBoxFlights_SelectedIndexChanged);
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(12, 301);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(651, 41);
+            this.buttonCalculate.TabIndex = 9;
+            this.buttonCalculate.Text = "Добавить в корзину";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // listBoxTickets
+            // 
+            this.listBoxTickets.FormattingEnabled = true;
+            this.listBoxTickets.ItemHeight = 20;
+            this.listBoxTickets.Location = new System.Drawing.Point(12, 352);
+            this.listBoxTickets.Name = "listBoxTickets";
+            this.listBoxTickets.Size = new System.Drawing.Size(650, 224);
+            this.listBoxTickets.TabIndex = 15;
+            // 
+            // labelClass
+            // 
+            this.labelClass.AutoSize = true;
+            this.labelClass.Location = new System.Drawing.Point(12, 55);
+            this.labelClass.Name = "labelClass";
+            this.labelClass.Size = new System.Drawing.Size(69, 20);
+            this.labelClass.TabIndex = 2;
+            this.labelClass.Text = "Размер:";
+            // 
+            // comboBoxClass
+            // 
+            this.comboBoxClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxClass.Location = new System.Drawing.Point(457, 52);
+            this.comboBoxClass.Name = "comboBoxClass";
+            this.comboBoxClass.Size = new System.Drawing.Size(200, 28);
+            this.comboBoxClass.TabIndex = 3;
+            // 
+            // pictureBoxProduct
+            // 
+            this.pictureBoxProduct.Location = new System.Drawing.Point(457, 105);
+            this.pictureBoxProduct.Name = "pictureBoxProduct";
+            this.pictureBoxProduct.Size = new System.Drawing.Size(199, 175);
+            this.pictureBoxProduct.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxProduct.TabIndex = 16;
+            this.pictureBoxProduct.TabStop = false;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(22, 105);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(51, 20);
+            this.labelDescription.TabIndex = 17;
+            this.labelDescription.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 586);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(652, 41);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Купить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(675, 639);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.pictureBoxProduct);
+            this.Controls.Add(this.listBoxTickets);
+            this.Controls.Add(this.labelFlight);
+            this.Controls.Add(this.comboBoxFlights);
+            this.Controls.Add(this.labelClass);
+            this.Controls.Add(this.comboBoxClass);
+            this.Controls.Add(this.buttonCalculate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Встречаю по одежке";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private ListBox listBoxTickets;
+        private Label labelClass;
+        private ComboBox comboBoxClass;
+        private PictureBox pictureBoxProduct;
+        private Label labelDescription;
+        private Button button1;
+    }
+}
---
+++ Встречают по одежке/Встречают по одежке/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Встречают_по_одежке
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                string flightType = comboBoxFlights.SelectedItem.ToString();
+                string classType = comboBoxClass.SelectedItem.ToString();
+
+                // Получение базовой цены
+                double basePrice = GetBasePrice(classType);
+                double baseCity = GetCity(flightType);
+                double totalPrice = 0;
+
+                // Расчёт стоимости для взрослых
+                totalPrice += baseCity;
+                
+
+                // Формирование корзины
+                string ticketInfo = $"Одежда: {flightType}";
+               
+                ticketInfo += $", Размер: {classType}, Итог: {totalPrice:F2} руб.";
+                listBoxTickets.Items.Add(ticketInfo);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка ввода данных: " + ex.Message, "Ошибка");
+            }
+        }
+
+        // Метод для отображения размера одежды
+        private double GetBasePrice(string classType)
+        {
+            switch (classType)
+            {
+                case "S":
+                  
+                case "M":
+                   
+                case "L":
+                    
+                case "X":
+                    return 0;
+                default:
+                    throw new Exception("Не выбраны данные для добавки в корзину.");
+            }
+        }
+
+        // Метод для отображения списка одежды
+        private double GetCity(string city)
+        {
+            switch (city)
+            {
+                case "Кофта":
+                    return 5000;
+                case "Брюки":
+                    return 8000;
+                case "Футболка":
+                    return 15000;
+                case "Куртка":
+                    return 25000;
+                case "Майка":
+                    return 25000;
+                default:
+                    throw new Exception("Некорректный выбор одежды.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxFlights.Items.AddRange(new string[] { "","Кофта", "Брюки", "Футболка", "Куртка", "Майка" });
+            comboBoxClass.Items.AddRange(new string[] {"", "S", "M", "L", "X" });
+            comboBoxFlights.SelectedIndex = 0;
+            comboBoxClass.SelectedIndex = 0;
+
+
+            
+
+            pictureBoxProduct.Image = null; // Начальное изображение
+            labelDescription.Text = "Описание товара будет отображаться здесь.";
+        }
+
+        private void comboBoxFlights_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem = comboBoxFlights.SelectedItem.ToString();
+            switch (selectedItem)
+            {
+                case "Кофта":
+                    pictureBoxProduct.Image = Properties.Resources.Кофта; // Пример пути к изображению
+                    labelDescription.Text = "Уютная кофта с капюшоном для холодной погоды.";
+                    break;
+                case "Брюки":
+                    pictureBoxProduct.Image = Properties.Resources.Брюки;
+                    labelDescription.Text = "Элегантные брюки для офиса или повседневной носки.";
+                    break;
+                case "Футболка":
+                    pictureBoxProduct.Image = Properties.Resources.Футболка;
+                    labelDescription.Text = "Легкая футболка для жарких дней, с ярким принтом.";
+                    break;
+                case "Куртка":
+                    pictureBoxProduct.Image = Properties.Resources.Куртка;
+                    labelDescription.Text = "Зимняя куртка с утеплителем, подходящая для холодных дней.";
+                    break;
+                case "Майка":
+                    pictureBoxProduct.Image = Properties.Resources.Майка;
+                    labelDescription.Text = "Удобные кроссовки, подходящие на любой случай жизни.";
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBoxTickets.Items.Count == 0)
+                {
+                    MessageBox.Show("Корзина пуста. Пожалуйста, добавьте товары перед покупкой.", "Ошибка");
+                    return;
+                }
+
+                // Генерация информации о заказе
+                string orderNumber = Guid.NewGuid().ToString().Substring(0, 8); // Уникальный номер заказа
+                DateTime orderDate = DateTime.Now; // Текущая дата
+                string orderItems = string.Join("\n", listBoxTickets.Items.Cast<string>()); // Состав заказа
+                double totalPrice = CalculateTotalPrice(); // Подсчёт общей суммы
+                string pickupPoint = "Пункт выдачи №1, ул. Сафонова, д. 5"; // Условный пункт выдачи
+                DateTime deliveryDate = orderDate.AddDays(3); // Срок выполнения заказа (например, 3 дня)
+                string pickupCode = new Random().Next(100000, 999999).ToString(); // Генерация кода получения
+
+                // Формирование текста для MessageBox
+                string message = $"Номер заказа: {orderNumber}\n" +
+                                 $"Дата заказа: {orderDate}\n" +
+                                 $"Состав заказа:\n{orderItems}\n" +
+                                 $"Общая сумма: {totalPrice:F2} руб.\n" +
+                                 $"Код получения: {pickupCode}\n" +
+                                 $"Пункт выдачи: {pickupPoint}\n" +
+                                 $"Срок исполнения: {deliveryDate:dd.MM.yyyy}";
+
+                // Вывод информации в MessageBox
+                MessageBox.Show(message, "Информация о заказе");
+
+                // Очистка корзины после оформления заказа
+                listBoxTickets.Items.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при оформлении заказа: " + ex.Message, "Ошибка");
+            }
+        }
+
+        // Метод для подсчёта общей суммы заказа
+        private double CalculateTotalPrice()
+        {
+            double totalPrice = 0;
+
+            foreach (string item in listBoxTickets.Items)
+            {
+                // Извлекаем цену из строки (предполагаем, что она в формате "... Итог: 12345.67 руб.")
+                int index = item.LastIndexOf("Итог: ");
+                if (index != -1)
+                {
+                    string priceText = item.Substring(index + 6).Replace(" руб.", "");
+                    if (double.TryParse(priceText, out double price))
+                    {
+                        totalPrice += price;
+                    }
+                }
+            }
+
+            return totalPrice;
+        }
+    }
+}
